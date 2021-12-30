dc5ec6beb3207f96e86a960bea1a474c6d5a8574
Malihassan <momenalhendawy@gmail.com>
2021-12-30 20:00:36 +0200

    fix another env proplem

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@
       }, 
     reactStrictMode: true,  
     env: {
-        REACT_APP_GOOGLE_CLIENT_ID: '413684897547-2joj0mh2sien6if3m6jp3qsjuojce9v4.apps.googleusercontent.com',
+        REACT_APP_GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     },     
   }
   
